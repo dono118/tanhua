89ff6b3bbc4e2365ccb472d6b47bd0a3fbbb3488
dono118 <dono118@126.com>
2022-04-18 21:32:39 +0800

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+}
---
+++ App.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const App = () => (
+  <View>
+    <Text>Hello, World!</Text>
+  </View>
+)
+export default App
