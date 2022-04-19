df551acdfae621665efc03fa492f7c1383ea0941
dono118 <dono118@126.com>
2022-04-19 20:41:18 +0800

    样式-屏幕宽高

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
-import {View, Text} from 'react-native'
+import {View, Text, Dimensions} from 'react-native'
 import React from 'react'
 
+const screenWidth = Math.round(Dimensions.get('window').width)
+const screenHeight = Math.round(Dimensions.get('window').height)
+
+/*
+  1. 在rn中容器的默认布局方式是 flex
+  2. 内部的子控件默认排列方式是纵向排列 flex-direction: column;
+  3. 在rn中样式没有继承
+  4. 单位 不用加px,vw,vh 百分比可以 "50%"
+  5. Dimensions 获取屏幕尺寸
+*/
 const App = () => (
-  <View>
-    <Text>Hello, World!</Text>
+  <View
+    style={{
+      backgroundColor: 'aqua',
+      flexDirection: 'row',
+      width: '50%',
+      height: '50%'
+    }}>
+    <Text style={{color: 'red', fontSize: 50}}>Hello, World!</Text>
+    <Text>React Native</Text>
+    <View
+      style={{
+        width: screenWidth,
+        height: screenHeight / 2,
+        backgroundColor: 'tan'
+      }}>
+      <Text>屏幕宽度和高度的一半</Text>
+    </View>
   </View>
 )
 export default App
