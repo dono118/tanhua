80b0798ee8964ed46ec5d688a23ef698a2905cff
dono118 <dono118@126.com>
2022-04-20 20:55:06 +0800

    TouchableOpacity组件

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import {View, Text} from 'react-native'
+import {TouchableOpacity, Text} from 'react-native'
 import React from 'react'
 
-/*
-  1. 在rn中容器的默认布局方式是 flex
-  2. 内部的子控件默认排列方式是纵向排列 flex-direction: column;
-  3. 在rn中样式没有继承
-  4. 单位 不用加px,vw,vh 百分比可以 "50%"
-  5. Dimensions 获取屏幕尺寸
-*/
+const handlePress = () => {
+  alert('Hi')
+}
+
 const App = () => (
-  <View>
-    <Text
-      style={{
-        backgroundColor: 'aqua',
-        transform: [{translateY: 200}, {scale: 2}]
-      }}>
-      Hello, World!
-    </Text>
-  </View>
+  <TouchableOpacity onPress={handlePress} activeOpacity={0.5}>
+    <Text>Touch Me</Text>
+  </TouchableOpacity>
 )
 export default App
