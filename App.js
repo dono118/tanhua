ad1bd8d5a78835e4f50dc2490ebe6b3a0cb8ef50
dono118 <dono118@126.com>
2022-04-23 11:24:55 +0800

    TextInput标签

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import { ImageBackground, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
 import React from 'react'
 
+const handleChangeText = text => {
+  alert(text)
+}
+
 const App = () => (
-  <ImageBackground
-    source={require('./images/1.jpeg')}
-    style={{ width: '100%', height: '100%' }}>
-    <Text
-      style={{
-        color: 'maroon',
-        fontSize: 22,
-        textAlign: 'center',
-        top: '30%'
-      }}>
-      Variety is the spice of life.
-    </Text>
-  </ImageBackground>
+  <View>
+    <TextInput
+      style={{ backgroundColor: '#F5DE83', paddingHorizontal: 12 }}
+      onChangeText={handleChangeText}
+      placeholder="请输入"
+    />
+  </View>
 )
+
 export default App
