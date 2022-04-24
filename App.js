af060023c21e04a2426309b813977052bb1dc318
dono118 <dono118@126.com>
2022-04-24 21:17:52 +0800

    插值表达式

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import { View, TextInput } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
-const handleChangeText = text => {
-  alert(text)
+const obj = {
+  name: '张三',
+  age: 25
 }
-
+const arr = ['apple', 'orange', 'banana']
 const App = () => (
   <View>
-    <TextInput
-      style={{ backgroundColor: '#F5DE83', paddingHorizontal: 12 }}
-      onChangeText={handleChangeText}
-      placeholder="请输入"
-    />
+    <Text>姓名：{obj.name}</Text>
+    <Text>年龄：{obj.age}</Text>
+    {arr.map((v, i) => (
+      <View key={i} style={{ backgroundColor: 'aqua' }}>
+        <Text>{v}</Text>
+      </View>
+    ))}
   </View>
 )
 
